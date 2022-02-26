82dc9e004beb2c78d441b2ca109e48dc1ef327ad
KJJ924 <dkansk924@naver.com>
2022-02-26 20:26:11 +0900

    ex tasklet

--- src/main/java/com/jaejoon/batch/spring_batch_study/job/SimpleJobTest.java
+++ src/main/java/com/jaejoon/batch/spring_batch_study/job/SimpleJobTest.java
@@ -1,38 +1,34 @@
 package com.jaejoon.batch.spring_batch_study.job;
 
-
+import com.jaejoon.batch.spring_batch_study.tasklet.SimpleTasklet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class SimpleJobConfiguration {
+public class SimpleJobTest {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep1())
+    public Job simpleJob2() {
+        return jobBuilderFactory.get("simpleJob2")
+            .start(simpleStep2())
             .build();
     }
 
     @Bean
-    public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1")
-            .tasklet((contribution, chunkContext) -> {
-                log.info(">>>This is Step1");
-                return RepeatStatus.FINISHED;
-            })
+    public Step simpleStep2() {
+        return stepBuilderFactory.get("simpleStep2")
+            .tasklet(new SimpleTasklet())
             .build();
     }
 }
---
+++ src/main/java/com/jaejoon/batch/spring_batch_study/tasklet/SimpleTasklet.java
@@ -0,0 +1,18 @@
+package com.jaejoon.batch.spring_batch_study.tasklet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+@Slf4j
+public class SimpleTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+        throws Exception {
+        log.info("simpleTasklet start");
+        return RepeatStatus.FINISHED;
+    }
+}
